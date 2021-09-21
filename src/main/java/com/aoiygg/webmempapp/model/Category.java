d05e966ce4c1d98353f9fdfa22d06e9fb45ff28b
Souhei-H <souhei.hosoi.se@gmail.com>
2021-09-21 15:53:46 +0900

    no comments

--- src/main/java/com/aoiygg/webmempapp/model/Category.java
+++ src/main/java/com/aoiygg/webmempapp/model/Category.java
@@ -2,16 +2,40 @@ package com.aoiygg.webmempapp.model;
 
 import javax.persistence.*;
 
-//@Entity
-//@Table(name = "category")
+@Entity
+@Table(name = "category")
 public class Category {
 
-//    @Id
-//    @GeneratedValue
-//    private Long id;
-//    @ManyToOne
-//    private Notepad notepad;
-//    private String name;
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long categoryId;
+    @Column(name = "mail_address")
+    private String mailAddress;
+    @Column(name = "category_name")
+    private String categoryName;
 
+    public Long getCategoryId() {
+        return categoryId;
+    }
 
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
--- src/main/resources/static/javascript/editNotepad.js
+++ src/main/resources/static/javascript/editNotepad.js
@@ -1,3 +1,5 @@
+let categoryList = [];
+
 function saveNotepad() {
     let titleEditor = document.getElementById("title-editor");
     let bodyEditor = document.getElementById("body-editor");
@@ -17,4 +19,30 @@ function resetNotepad() {
 
     titleEditor.innerText = null;
     bodyEditor.innerText = null;
+}
+
+function addCategory() {
+    let categoryListContainer = document.getElementById('category-list-container');
+    let categoryEditor = document.getElementById('category-editor');
+    categoryList.push(categoryEditor.innerText);
+    let newCategoryContainer = document.createElement('div');
+    newCategoryContainer.classList.add('category-container');
+    let newCategory = document.createElement('div');
+    newCategory.innerText = categoryEditor.innerText;
+    let removeCategory = document.createElement('div');
+    removeCategory.innerText = '×';
+    removeCategory.addEventListener('click', function() {
+        removeCategoryFun(newCategory.innerText);
+    }, false);
+    newCategoryContainer.insertBefore(removeCategory, newCategoryContainer.firstChild)
+    newCategoryContainer.insertBefore(newCategory, newCategoryContainer.firstChild)
+    categoryListContainer.insertBefore(newCategoryContainer, categoryListContainer.firstChild);
+    categoryEditor.innerText = null;
+}
+
+function removeCategoryFun(category) {
+    let index = categoryList.indexOf(category);
+    categoryList.splice(index, 1);
+    let categoryListContainer = document.getElementById('category-list-container');
+    categoryListContainer.removeChild(categoryListContainer.children[categoryListContainer.childElementCount - index - 1]);
 }
