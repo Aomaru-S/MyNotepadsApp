eec1260eec68539165931fce3bd355f18608ee9e
Souhei-H <souhei.hosoi.se@gmail.com>
2021-10-04 19:07:05 +0900

    no comments

--- src/main/resources/static/javascript/myNotepads.js
+++ src/main/resources/static/javascript/myNotepads.js
@@ -1,5 +1,4 @@
 function submit(e) {
     document.getElementById("id-form").value = e.children.item(0).innerText;
-    alert(document.getElementById(document.getElementById("id-form").value))
-    // document.getElementById("edit-form").submit();
+    document.getElementById("edit-form").submit();
 }
